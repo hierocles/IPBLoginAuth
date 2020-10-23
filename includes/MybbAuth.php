5683eb2feb7d3f29903988fd202dba3c934cc343
Dylan H <4733259+hierocles@users.noreply.github.com>
2020-10-22 23:59:43 -0400

    Fixed missing cfg declaration. Added multiple mappings in MybbGroupMap.

--- includes/MybbAuth.php
+++ includes/MybbAuth.php
@@ -200,9 +200,12 @@ class MybbAuth
                             if (is_array($groupmap)) {
                                 foreach ($groupmap as $ug_wiki => $ug_mybb) {
                                     $user_has_ug = in_array($ug_wiki, $user->getEffectiveGroups());
-                                    if (in_array($ug_mybb, $groups) && !$user_has_ug) {
+                                    $user_needs_ug = !empty(
+                                        array_uintersect((array)$ug_mybb, $groups, 'version_compare')
+                                    );
+                                    if ($user_needs_ug && !$user_has_ug) {
                                         $user->addGroup($ug_wiki);
-                                    } elseif (!in_array($ug_mybb, $groups) && $user_has_ug) {
+                                    } elseif (!$user_needs_ug && $user_has_ug) {
                                         $user->removeGroup($ug_wiki);
                                     }
                                 }
@@ -227,6 +230,7 @@ class MybbAuth
      */
     public static function userExists($username)
     {
+        $cfg = MybbAuth::getConfig();
         $sql = MybbAuth::getSQL();
         try {
             if ($sql->connect_errno) {
